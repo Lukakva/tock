5add6c64f5b6d26ae303b9f2081ec7ec8550a0eb
Luka Kvavilashvili <luka.kvavilashvili@deckers.com>
2023-05-01 00:30:38 +0200

    Progress

--- capsules/extra/src/fatfs/driver.rs
+++ capsules/extra/src/fatfs/driver.rs
@@ -16,7 +16,7 @@ use kernel::hil;
 use kernel::process::Process;
 use kernel::processbuffer::{ReadableProcessBuffer, WriteableProcessBuffer};
 use kernel::syscall::{CommandReturn, SyscallDriver};
-use kernel::utilities::cells::{OptionalCell, TakeCell};
+use kernel::utilities::cells::{MapCell, OptionalCell, TakeCell};
 use kernel::{ErrorCode, ProcessId};
 
 /// Syscall driver number.
@@ -24,10 +24,13 @@ pub const DRIVER_NUM: usize = capsules_core::driver::NUM::FatFS as usize;
 
 /// Ids for read-only allow buffers
 mod ro_allow {
-    /// ID of the buffer shared by the user space application which we can read from.
-    pub const BUFFER: usize = 0;
+    /// User process uses this buffer when it wants to write to a file.
+    pub const WRITE_BUFFER: usize = 0;
+    /// User process uses this buffer to give the driver the name of
+    /// the file it wants to perform the operation on.
+    pub const FILE_NAME: usize = 1;
     /// The number of allow buffers the kernel stores for this grant
-    pub const COUNT: u8 = 1;
+    pub const COUNT: u8 = 2;
 }
 
 /// Ids for read-write allow buffers
@@ -39,15 +42,21 @@ mod rw_allow {
 }
 
 /// Available syscalls for this driver.
-mod Cmd {
+mod cmd {
     /// Inits the driver to "work for" some process.
     /// Should also pass the volume identifier that the process wants to use.
     /// Volume identifier should be 0, 1, 2, or 3. Only 4 partitions are supported.
     pub const INIT: usize = 0;
+    /// Opens the root directory.
+    pub const OPEN_ROOT_DIR: usize = 1;
     /// Open a directory. Userspace should be cognisant of the open-dir limit.
-    pub const OPEN_DIR: usize = 1;
+    pub const OPEN_DIR: usize = 2;
     /// Opens a file in the specified directory.
-    pub const OPEN_FILE: usize = 2;
+    pub const OPEN_FILE: usize = 3;
+
+    /// Tells the driver that the process is done using the driver.
+    /// Essentially un-reserves it.
+    pub const DONE: usize = 8;
 }
 
 // We're not really storing anything in the Grant segment of a process.
@@ -56,6 +65,8 @@ struct App;
 
 const MAX_DIRS: usize = 8;
 const MAX_FILES: usize = 8;
+
+pub const FILENAME_BUFFER: &'static str = "12345678.123";
 
 /// Struct that stores the state of the Fat32 driver.
 /// Stores information about the current process that the driver is serving.
@@ -69,13 +80,20 @@ pub struct FatFsDriver<D: BlockDevice + 'static, T: TimeSource + 'static> {
         AllowRwCount<{ rw_allow::COUNT }>,
     >,
     controller: TakeCell<'static, Controller<D, T, MAX_DIRS, MAX_FILES>>,
+
+    filename_buffer: TakeCell<'static, str>,
+
+    // Values below keep track of userspace requests and are not part of
+    // initializing the driver, therefore they are optional.
     /// The current volume (partition) the driver is working in.
-    volume: OptionalCell<Volume>,
-    // Keep track of the structs returned by the controller.
-    // We need those to read files in directories.
+    volume: MapCell<Volume>,
+    /// Directory Descriptor table. Every directory opened by the user process
+    /// has a corresponding int ID.
     directories: [OptionalCell<Directory>; MAX_DIRS],
-    // Keep track of the structs returned by the controller.
+    /// File Descriptor table. Every file opened by the user process
+    /// has a corresponding int ID.
     files: [OptionalCell<File>; MAX_FILES],
+    /// The process that reserved the driver.
     current_process: OptionalCell<ProcessId>,
 }
 
@@ -84,12 +102,11 @@ impl<D: BlockDevice, T: TimeSource> FatFsDriver<D, T> {
     /// Returns Ok() iff the process making the syscall is the one which reserved the driver.
     /// Returns Fail(ErrorCode::RESERVE) if no process has reserved the driver,
     /// or Fail(ErrorCode::BUSY) if another process has reserved the driver.
-    #[inline]
-    fn process_can_request(&self, process_id: ProcessId) -> Result<(), ErrorCode> {
+    fn has_reservation(&self, process_id: &ProcessId) -> Result<(), ErrorCode> {
         self.current_process
             // If no current process is set, indicate that the current calling process can
             // request to reserve the driver.
-            .map_or(Err(ErrorCode::RESERVE), |id| match *id == process_id {
+            .map_or(Err(ErrorCode::RESERVE), |id| match id == process_id {
                 true => Ok(()),
                 // Indicate that another process has reserved the driver.
                 false => Err(ErrorCode::BUSY),
@@ -103,59 +120,130 @@ impl<D: BlockDevice, T: TimeSource> FatFsDriver<D, T> {
         // Maybe the process reserved the driver, but didn't do anything.
         match self.volume.take() {
             Some(volume) => {
-                for dir in self.directories.iter() {
-                    dir.clear();
-                }
+                self.controller.map(|controller| {
+                    for dir in self.directories.iter() {
+                        // Using .take().map() allows us to clear the `OptionalCell` and
+                        // at the same time, if there was a directory struct in the cell,
+                        // use the value.
+                        dir.take().map(|dir| {
+                            controller.close_dir(&volume, dir);
+                        });
+                    }
 
-                for file in self.files.iter() {
-                    file.clear();
-                }
+                    for file in self.files.iter() {
+                        file.take().map(|file| {
+                            controller.close_file(&volume, file).ok();
+                        });
+                    }
 
-                self.current_process.clear();
+                    self.current_process.clear();
+                });
             }
             None => {}
-        }
+        };
     }
 
     /// Handles the INIT syscall.
-    #[inline]
-    fn syscall_init(&self, process_id: ProcessId, partition_index: usize) -> CommandReturn {
-        let current_process_id = self.current_process.take();
-        match current_process_id {
-            // Some process reserved the driver.
-            Some(id) => {
-                // Put the ID back in the cell.
-                self.current_process.replace(id);
-                // If this process is the current process that is making the syscall
-                // inform the process that it already has a reservation with the driver.
-                if id == process_id {
-                    CommandReturn::failure(ErrorCode::ALREADY)
-                } else {
-                    CommandReturn::failure(ErrorCode::BUSY)
+    fn syscall_init(&self, process_id: ProcessId, partition_index: usize) -> Result<(), ErrorCode> {
+        // Init call is a little unique, but we can still re-use the function below
+        match self.has_reservation(&process_id) {
+            // If Ok() is returned, the current process has already reserved
+            // the driver so INIT syscall makes no sense.
+            Ok(_) => Err(ErrorCode::ALREADY),
+            Err(code) => match code {
+                // Can't init for this process. other process has reserved it.
+                ErrorCode::BUSY => Err(code),
+                // This error means that no reservation has been made so far, so init can happen.
+                ErrorCode::RESERVE => {
+                    // No process has a reservation.
+                    self.controller
+                        .map(
+                            |controller| match controller.get_volume(VolumeIdx(partition_index)) {
+                                Ok(volume) => {
+                                    self.current_process.replace(process_id);
+                                    self.volume.replace(volume);
+                                    Ok(())
+                                }
+                                Err(_err) => Err(ErrorCode::FAIL),
+                            },
+                        )
+                        .unwrap_or(Err(ErrorCode::FAIL))
                 }
-            }
-            // No reservation.
-            None => {
-                // No process has a reservation.
-                self.controller
-                    .map(
-                        |controller| match controller.get_volume(VolumeIdx(partition_index)) {
-                            Ok(volume) => {
-                                self.current_process.replace(process_id);
-                                self.volume.replace(volume);
-                                CommandReturn::success()
-                            }
-                            Err(_err) => CommandReturn::failure(ErrorCode::FAIL),
-                        },
-                    )
-                    .unwrap_or(CommandReturn::failure(ErrorCode::FAIL))
-            }
+                // Should never happen.
+                code => Err(code),
+            },
         }
     }
 
+    /// Opens the root directory and stores it in directory descriptor 0.
+    fn syscall_open_root_dir(&self, process_id: ProcessId) -> Result<u32, ErrorCode> {
+        // If directory descriptor 0 is already taken, it's root.
+        if self.directories[0].is_some() {
+            return Err(ErrorCode::ALREADY);
+        }
+
+        self.volume.take().map_or(Err(ErrorCode::FAIL), |volume| {
+            let result = self
+                .controller
+                .map(|controller| match controller.open_root_dir(&volume) {
+                    Ok(root_directory) => {
+                        self.directories[0].replace(root_directory);
+                        // Convention is to send back the file/dir descriptor (int)
+                        // of whatever the userspace opened. For root dir it's always 0.
+                        Ok(0)
+                    }
+                    Err(err) => Err(ErrorCode::FAIL),
+                })
+                .unwrap_or(Err(ErrorCode::FAIL));
+
+            // Put back the volume struct.
+            self.volume.replace(volume);
+
+            return result;
+        })
+    }
+
     /// Handles the OPEN_DIR syscall.
-    #[inline]
-    fn syscall_opendir(&self, process_id: ProcessId) {}
+    fn syscall_opendir(
+        &self,
+        process_id: ProcessId,
+        parent_dir_id: usize,
+    ) -> Result<u32, ErrorCode> {
+        // Below is the unpacking hell. Essentially this code, in a safe manner
+        // 1. Ensures the directory ID given by the process is valid.
+        // 2. Retrieves the controller.
+        // 3. Retrieves the current volume the process is working with.
+        // 4. Retrieves the string slice from the user process that contains
+        // the name of the dir.
+        self.directories
+            .get(parent_dir_id)
+            .map_or(Err(ErrorCode::INVAL), |cell| {
+                cell.take().map_or(Err(ErrorCode::INVAL), |parent_dir| {
+                    self.controller.map_or(Err(ErrorCode::FAIL), |controller| {
+                        self.volume.take().map_or(Err(ErrorCode::FAIL), |volume| {
+                            // Retrieve the file name from userspace.
+                            let name = self.grants.enter(process_id, |app_data, kernel_data| {
+                                kernel_data
+                                    .get_readonly_processbuffer(ro_allow::FILE_NAME)
+                                    .and_then(|data| data.enter(|data| {}))
+                            });
+
+                            let result = match controller.open_dir(&volume, &parent_dir, "asd") {
+                                Ok(res) => Ok(1),
+                                Err(err) => Err(ErrorCode::INVAL),
+                            };
+
+                            // Put back
+                            self.volume.replace(volume);
+                            result
+                        })
+                    })
+                })
+            })
+    }
+
+    /// Handles the OPEN_FILE syscall.
+    fn syscall_open_file() {}
 }
 
 impl<D: BlockDevice, T: TimeSource> SyscallDriver for FatFsDriver<D, T> {
@@ -167,11 +255,31 @@ impl<D: BlockDevice, T: TimeSource> SyscallDriver for FatFsDriver<D, T> {
         process_id: ProcessId,
     ) -> CommandReturn {
         match command_num {
-            // Init syscall.
+            // Init syscall. Reserves the driver, if successful.
             // Treat the `r2` value as the ID of the partition that this process
             // wants to work with.
-            Cmd::INIT => self.syscall_init(process_id, r2),
-            Cmd::OPEN_DIR => self.syscall_opendir(process_id),
+            cmd::INIT => match self.syscall_init(process_id, r2) {
+                Ok(_) => CommandReturn::success(),
+                Err(code) => CommandReturn::failure(code),
+            },
+            command => {
+                // Otherwise, for any other syscall, there needs to be a reservation
+                // made by the process.
+                match self.has_reservation(&process_id) {
+                    Ok(_) => match command {
+                        cmd::OPEN_ROOT_DIR => match self.syscall_open_root_dir(process_id) {
+                            Ok(id) => CommandReturn::success_u32(id),
+                            Err(code) => CommandReturn::failure(code),
+                        },
+                        cmd::OPEN_DIR => match self.syscall_opendir(process_id, r2) {
+                            Ok(id) => CommandReturn::success_u32(id),
+                            Err(code) => CommandReturn::failure(code),
+                        },
+                        _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
+                    },
+                    Err(code) => CommandReturn::failure(code),
+                }
+            }
             _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
         }
     }
