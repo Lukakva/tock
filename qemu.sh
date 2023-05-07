fa4ba17cc8f7d3496bd2350c8b2d63ee2e4cbc24
Luka Kvavilashvili <lukakvavilashvili@gmail.com>
2023-05-07 02:11:16 +0200

    Fix

--- qemu.sh
+++ qemu.sh
@@ -1,20 +1,2 @@
-# ./tools/qemu/build/qemu-system-riscv32 -M sifive_e,revb=true -kernel ./target/riscv32imac-unknown-none-elf/release/hifive1.elf -nographic
-#!/bin/sh
- 
-git filter-branch --env-filter '
- 
-OLD_EMAIL="luka.kvavilashvili@deckers.com"
-CORRECT_NAME="Luka Kvavilashvili"
-CORRECT_EMAIL="lukakvavilashvili@gmail.com"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
+./tools/qemu/build/qemu-system-riscv32 -M sifive_e,revb=true -kernel ./target/riscv32imac-unknown-none-elf/release/hifive1.elf -nographic
+!/bin/sh
